faf674545826023aee3b1c8580c635f0757590d0
tregulovailmira <tregulovailmira@gmail.com>
2022-11-11 13:58:34 +0200

    refactor(EW-11): replace http with dio everywhere

--- lib/features/vocabulary/data/datasources/word_api_datasource.dart
+++ lib/features/vocabulary/data/datasources/word_api_datasource.dart
@@ -1,19 +1,17 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 
 import '../../../../core/errors/exceptions.dart';
 import '../models/word_description_model.dart';
 
-final baseUrl = dotenv.env['API_BASE_URL'];
+final baseUrl = dotenv.env['VOCABULARY_BASE_URL'];
 
 abstract class WordApiDataSource {
   Future<WordDescriptionModel> getWordDescription(String word);
 }
 
 class WordApiDataSourceImpl implements WordApiDataSource {
-  final http.Client client;
+  final Dio client;
 
   WordApiDataSourceImpl(this.client);
 
@@ -21,17 +19,14 @@ class WordApiDataSourceImpl implements WordApiDataSource {
   Future<WordDescriptionModel> getWordDescription(String word) async {
     final url = '$baseUrl/$word';
     try {
-      final response = await client.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        return WordDescriptionModel.fromJson(
-          (json.decode(response.body) as List).first,
-        );
-      } else {
-        final message = json.decode(response.body)['message'] as String;
-        throw ApiException(message, response.statusCode);
-      }
-    } on ApiException {
-      rethrow;
+      final response = await client.get(url);
+      return WordDescriptionModel.fromJson((response.data as List).first);
+    } on DioError catch (e) {
+      final errorMesage =
+          e.response != null ? e.response!.data['message'] : e.message;
+      final statusCode = e.response?.statusCode;
+
+      throw ApiException(errorMesage, statusCode);
     } catch (e) {
       throw Exception(e.toString());
     }
